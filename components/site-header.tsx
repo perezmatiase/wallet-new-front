f9346c50c3290dc4d77027f2638b57f35043877b
perezmatiase <perezmatiase@gmail.com>
2025-04-22 09:01:21 -0300

    set dark/light mode

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,8 +1,16 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
 
 export function SiteHeader() {
+  const { theme, setTheme } = useTheme()
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -22,6 +30,16 @@ export function SiteHeader() {
             >
               GitHub
             </a>
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={toggleTheme}
+            className="relative"
+          >
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
           </Button>
         </div>
       </div>
